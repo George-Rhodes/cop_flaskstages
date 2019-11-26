a45da5d3986d6ce756fbeb37f03b36e6b35769fc
BenHesketh21 <heskethben0@gmail.com>
2019-11-26 14:36:14 +0000

    Adding different stages

---
+++ stage-0/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return """<html>
+              <head>
+                <title>home page</title>
+              </head>
+              <body>
+                <h1>Home Page</h1>
+              </body>
+            </html>
+        """
+@app.route('/about')
+def about():
+    return """<html>
+              <head>
+                <title>about page</title>
+              </head>
+              <body>
+                <h1>About Page</h1>
+              </body>
+            </html>
+        """
+
+
+
