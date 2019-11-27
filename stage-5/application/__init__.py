47358c9af34e2ca3d85a34a4d842e0cc9cba3427
BenHesketh21 <heskethben0@gmail.com>
2019-11-27 15:21:05 +0000

    Added stage 5

---
+++ stage-5/application/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'YOU_SECRET_KEY'
+db = SQLAlchemy(app)
+
+from application import routes
