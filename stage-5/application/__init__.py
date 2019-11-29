bed98104592402e456c93ecb3b23515e9fcbeefb
BenHesketh21 <55387271+BenHesketh21@users.noreply.github.com>
2019-11-29 11:54:54 +0000

    Update __init__.py

--- stage-5/application/__init__.py
+++ stage-5/application/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'YOU_SECRET_KEY'
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 db = SQLAlchemy(app)
 
 from application import routes
